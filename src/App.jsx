5dc14af68847302aa71658fe43f72754ea2c230c
Array1994 <114093591+Array1994@users.noreply.github.com>
2023-06-07 05:48:56 -0700

    upate on recent 06/06

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,10 @@ console.log(count)
         <div id="menu">
             <p>Main</p>
             <ul>
-                <Link className="link"to="/" ><li className="list"><img className="list-icon" src="./public/dashboard.png"/>Dashboard</li></Link>
-                <Link className="link"to="/strategy" ><li className="list"><img className="list-icon" src="./public/strategy.png"/>Strategy</li></Link>
-                <Link className="link"to="/support" ><li className="list"><img className="list-icon" src="./public/support.png"/>Support</li></Link>
-                <Link className="link"to="/settings" ><li className="list"><img className="list-icon" src="./public/settings.png"/>Settings</li></Link>
+                <Link className="link"to="/" ><li className="list"><img className="list-icon" src="/dashboard.png"/>Dashboard</li></Link>
+                <Link className="link"to="/strategy" ><li className="list"><img className="list-icon" src="/strategy.png"/>Strategy</li></Link>
+                <Link className="link"to="/support" ><li className="list"><img className="list-icon" src="/support.png"/>Support</li></Link>
+                <Link className="link"to="/settings" ><li className="list"><img className="list-icon" src="/settings.png"/>Settings</li></Link>
 
             </ul>
         </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -23,19 +23,19 @@ function Header(){
         <div id="header">
                 <div className="logo">
                 <Link className="link"to="/"><h3>Flameego</h3></Link>
-                <img id="noti" onClick={toggle2} className="icon" src="./public/notification.png"/>
+                <img id="noti" onClick={toggle2} className="icon" src="notification.png"/>
                 </div>
                 <div id="account">
                     <img id='account-icon' src="/account.png"/>
-                    <span>Ayobami  <img id="down-arrow" onClick={toggle} src="./public/arrow-down.png"/></span>
+                    <span>Ayobami  <img id="down-arrow" onClick={toggle} src="./arrow-down.png"/></span>
                 </div>
                 
                 
         </div>
         <div id="drop" className={`drop-down ${drop ? "" : "hideen"} `}>
-                    <Link className="link" to="/settings"><p className="drop"><img  className="icon " src="./public/settings.png"/>  settings</p></Link>
-                    <Link className="link" to="/support"><p className="drop"><img className="icon " src="./public/support.png"/>   support</p></Link>
-                    <Link className="link"><p className="drop"><img className="icon " src="./public/settings.png"/>  logout</p></Link>
+                    <Link className="link" to="/settings"><p className="drop"><img  className="icon " src="/settings.png"/>  settings</p></Link>
+                    <Link className="link" to="/support"><p className="drop"><img className="icon " src="/support.png"/>   support</p></Link>
+                    <Link className="link"><p className="drop"><img className="icon " src="/settings.png"/>  logout</p></Link>
         </div>
         <div id="notification" className={`${noti ? "" : "hideen"} `}>
             <p>you have no notification</p>
